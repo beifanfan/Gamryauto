7f5c5870dd373eccc5cbb613ee765dc31b4c1226
beifanfan <72921582+beifanfan@users.noreply.github.com>
2021-08-29 00:23:14 +0800

    Delete Gen9.2 directory

--- Gen9.2/Chaoranclose.m
+++
@@ -1,3 +0,0 @@
-function Chaoranclose(hObject, eventdata, handles)
-assignin('base','statebase',0);
-end
--- Gen9.2/choiceread.m
+++
@@ -1,9 +0,0 @@
-function [choiceout,choicenum]=choiceread(text)
-choice=regexp(text{1,1}, '\s+', 'split');
-[~,choicenum]=size(choice);
-choiceout=zeros(1,choicenum);
-for i=1:choicenum
-    choiceout(i)=str2num(choice{1,i});
-end
-
-end
